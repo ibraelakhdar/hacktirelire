83ae99d5d23ab7a7808d25eef73a8e426d2ec168
elakhdari <ibrahim.elakhdar@pictime.com>
2017-05-20 06:37:50 +0200

    add autorisation

--- src/main/java/tirelire/piggybank/PiggyBank.java
+++ src/main/java/tirelire/piggybank/PiggyBank.java
@@ -6,26 +6,33 @@ import projectchild.ProjectChild;
 import tirelire.exception.CantRemoveMoney;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by pictime on 20/05/17.
  */
 public class PiggyBank implements Serializable {
 
-    private PiggyBank(){}
+
     private Integer amount = 0;
     private ArrayList<Transaction> history = new ArrayList<Transaction>();
     private ProjectChild project;
+    private Autorisation autorisation = new Autorisation(this.soldeAutorise);
+    private Integer soldeAutorise = 50;
     private static PiggyBank PIGGYBANK = new PiggyBank();
 
     public static PiggyBank getInstance()
     {
         return PIGGYBANK;
     }
+    private PiggyBank(){
 
+    }
     public Integer addMoney(Integer value){
         System.out.println(value);
         history.add(new Transaction(value,  (new SimpleDateFormat("MM-dd-yyyy").format(new Date())).toString(), TypeTransaction.DEPOT));
@@ -33,12 +40,16 @@ public class PiggyBank implements Serializable {
         updatePourcentageProject();
         return amount;    }
 
-    public Integer removeMoney(Integer value) throws CantRemoveMoney {
-        if( amount < value ) throw new CantRemoveMoney();
-        history.add(new Transaction(value, ((new SimpleDateFormat("MM-dd-yyyy").format(new Date())).toString()).toString(), TypeTransaction.RETRAIT));
 
-         amount-=value;
-        updatePourcentageProject();
+    public Integer removeMoney(Integer value) throws CantRemoveMoney, ParseException {
+        if( amount < value ) throw new CantRemoveMoney();
+        DateFormat format = new SimpleDateFormat("MMMM d, yyyy", Locale.FRANCE);
+        Date dateLastTransation = format.parse(history.get(history.size()-1).getDate());
+        if(new Date().getTime() - dateLastTransation.getTime() > 262974600 ) {
+            history.add(new Transaction(value, ((new SimpleDateFormat("MM-dd-yyyy").format(new Date())).toString()).toString(), TypeTransaction.RETRAIT));
+            amount -= value;
+            updatePourcentageProject();
+        }
         return amount;
     }
 
@@ -58,7 +69,7 @@ public class PiggyBank implements Serializable {
         this.history = history;
     }
     public void createProject(ProjectChild project) {
-        if(this.project==null)
+        if(this.project==null && project!=null )
             this.project = project;
     }
 
@@ -74,6 +85,23 @@ public class PiggyBank implements Serializable {
         if(this.project!=null)
             this.project.setPourcentage(this.amount/this.project.getAmount());
     }
+
+    public Autorisation getAutorisation() {
+        return autorisation;
+    }
+
+    public void setAutorisation(Autorisation autorisation) {
+        this.autorisation = autorisation;
+    }
+
+    public Integer getSoldeAutorise() {
+        return soldeAutorise;
+    }
+
+    public void setSoldeAutorise(Integer soldeAutorise) {
+        this.soldeAutorise = soldeAutorise;
+    }
+
     @Override
     public String toString() {
         return "PiggyBank{" +
